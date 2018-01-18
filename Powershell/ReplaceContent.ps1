58cd61ee56fb9a14ba15becf90717b303c6d45ef
NaotoHama <wmkfkkt.dev@gmail.com>
2018-01-19 01:09:42 +0900

    Add ReplaceContent.ps1

---
+++ Powershell/ReplaceContent.ps1
@@ -0,0 +1,22 @@
+Param(
+    [parameter(mandatory=$true)][string]$BeforeStr,
+    [parameter(mandatory=$true)][string]$AfterStr,
+    [parameter(mandatory=$true)][string]$Comment
+)
+$Date = Date -Format "yyy/MM/dd"
+
+$AfterStr = @"
+'/ FIX:$Date ${Comment}
+${AfterStr}
+"@
+
+$FileList = Get-ChildItem */*
+
+foreach($File in $FileList){
+    $Content = Get-Content $File
+    $LineNumberList = Select-String $BeforeStr $File | Select LineNumber
+    foreach($LineNumber in $LineNumberList){
+        $Content[[int]$LineNumber.LineNumber-1] = $Content[[int]$LineNumber.LineNumber-1] -Replace  $BeforeStr, $AfterStr
+        $Content| Out-File $File -Encoding utf8
+    }
+}
